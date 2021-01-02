e7d5125decd1b8cce3f1e58de4e87a3eab800ff3
Tang <329937872@qq.com>
2021-01-02 23:36:45 +0800

    跳过java的target

--- HelloJava/src/main/java/ice/CommonServiceI.java
+++ HelloJava/src/main/java/ice/CommonServiceI.java
@@ -3,6 +3,8 @@ package ice;
 import ice.meta._CommonServiceDisp;
 
 public class CommonServiceI extends _CommonServiceDisp {
+    public static final long serialVersionUID = -1562167471L;
+
     public void request(String s, Ice.Current current) {
         System.out.println(s);
     }
