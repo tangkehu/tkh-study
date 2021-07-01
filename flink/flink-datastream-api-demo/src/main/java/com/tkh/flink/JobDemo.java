82e8e7d6624d6caa1189b16a782aad1fb823a703
tangkehu-mi <329937872@qq.com>
2021-07-01 14:16:29 +0800

    整理依赖，特别是日志，写了一个 WordCount 示例 。

---
+++ flink/flink-datastream-api-demo/src/main/java/com/tkh/flink/JobDemo.java
@@ -0,0 +1,37 @@
+package com.tkh.flink;
+
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class JobDemo {
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env.setRuntimeMode(RuntimeExecutionMode.STREAMING);
+
+        env.readTextFile("pom.xml")
+                .flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+                    @Override
+                    public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                        for (String word : value.split(" ")) {
+                            if (!word.trim().isEmpty()) {
+                                out.collect(Tuple2.of(word.trim(), 1));
+                            }
+                        }
+                    }
+                })
+                .keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+                    @Override
+                    public String getKey(Tuple2<String, Integer> value) throws Exception {
+                        return value.f0;
+                    }
+                })
+                .sum(1)
+                .print();
+        env.execute("demo");
+    }
+}
